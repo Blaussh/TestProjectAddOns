336badd8a150d7ccdbaf38813542a48561b88dcc
Blaussh <Shai.gm@gmail.com>
2018-12-23 18:36:49 +0200

    added the stop function

--- ServiceControllerAddOn/ServiceControllerAddOn/ServiceController.cs
+++ ServiceControllerAddOn/ServiceControllerAddOn/ServiceController.cs
@@ -4,12 +4,13 @@ using TestProject.SDK.Addons.Actions;
 using TestProject.SDK.Addons.Helpers;
 using TestProject.SDK.Common.Attributes;
 using TestProject.SDK.Common.Enums;
-
+using System.ServiceProcess;
 namespace ServiceControllerAddOn
 {
     [Action(Name = "Stop Service", Description = "Stops a windows service of your choice")]
     public class StopServiceAction : IGenericAction
     {
+
         [Parameter]
         public string ServiceName;
 
@@ -19,9 +20,18 @@ namespace ServiceControllerAddOn
         [Parameter(Description = "Stop result", Direction = ParameterDirection.Output)]
         public string Result;
 
+        public enum ServiceCustomCommands
+        {
+            StopWorker = 128,
+            RestartWorker,
+            CheckWorker
+        }
+
         public ExecutionResult Execute(GenericAddonHelper helper)
         {
-            Result = $"We are stopping {ServiceName} and waiting {WaitingTime}";
+            ServiceController service = new ServiceController("AlgoAnalyzer");
+            Result = $"We are stopping AlgoAnalyzer and waiting {WaitingTime} - {service.Status}";
+            service.Stop();
             helper.Reporter.Result = $"Attention! {Result}";
 
             return ExecutionResult.Passed;
